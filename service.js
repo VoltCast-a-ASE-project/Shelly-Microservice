4434e65bdbea31095f7e03546112f14152e9f5a8
laulaobm <laura@diehofers.at>
2025-12-09 16:24:32 +0100

    remove: cors

--- service.js
+++ service.js
@@ -12,15 +12,6 @@ const port = 8083;
 
 app.use(express.json());
 
-// checks for specific methods, headers and a certain origin
-app.use(cors({
-  origin: 'http://localhost:8080',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  //allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  //credentials: true
-}));
-
-
 app.get('/hello', (req, res) => {
   res.status(200).json({ message: 'Hello from Shelly Microservice!' });
 });
